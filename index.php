5b5a8e4fa0b16e3d6dec720e7d47f4f398a2c291
yuniaremeilia2048 <81125278+yuniaremeilia2048@users.noreply.github.com>
2021-10-01 09:15:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DetikInet</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <nav>
+        <p class="nama-web">DetikInet</p>
+        <ul>
+            <li><a href="">Kuliner</a></li>
+            <li><a href="">Olahraga</a></li>
+            <li><a href="">Kesenian</a></li>
+            <li><a href="">Bisnis</a></li>
+            <li><a href="">Teknologi</a></li>
+            <li><a href="">Selebriti</a></li>
+            <li><a href="">Wisata</a></li>
+            <li><a href="">Kecantikan</a></li>
+            <li><a href="">Tips & Trik</a></li>
+        </ul>
+    </nav>    
+    <header>
+        <p class="header"><?php $heading_text = "Kalahkan YouTube, <br/> Instagram Raup Rp 274 Triliun";
+        echo $heading_text;?></p>
+        </header>
+    <article>
+    <main>
+        <p class="date"><?php $date_text = "detikInet | Rabu, 05 Feb 2020 14.01 WIB";
+        echo $date_text;?></p>
+        <img src="https://images.unsplash.com/photo-1521302200778-33500795e128?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80" alt="">
+        <img src="https://images.unsplash.com/photo-1616469829935-c2f33ebd89b8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80" alt="">
+        <p class="konten"><?php $body_text = "Instagram dilaporkan meraup cuan sebesar USD 20 miliar selama 2019. Angka ini jauh menyalip pendapatan yang diraih YouTube di periode yang sama.";
+        echo $body_text;?></p>
+    </main>
+    </article>
+    <footer><center>
+        Copyright © 2021 ~ Yuniar[2048]
+    </center></footer>
+</body>
+</html>
